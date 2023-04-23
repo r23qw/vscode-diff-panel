8cbffa0080a9f9cd88d8d75f956fd742875b5762
aaron00101010 <aaron00101010@gmail.com>
2023-04-24 01:30:17 +0800

    chore: stash

--- webview-ui/src/activity-bar/main.ts
+++ webview-ui/src/activity-bar/main.ts
@@ -2,6 +2,7 @@ import 'virtual:uno.css'
 import { createApp } from 'vue'
 import { allComponents, provideVSCodeDesignSystem } from '@vscode/webview-ui-toolkit'
 import App from './App.vue'
+import '../../public/codicon.css'
 
 // TODO: reigster components on-demand
 provideVSCodeDesignSystem().register(allComponents)
