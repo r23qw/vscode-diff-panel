c795b59d2a4b2e7791a5279ff0ac0a2ba8be015a
aaron00101010 <aaron00101010@gmail.com>
2023-04-25 00:44:55 +0800

    chore: clean

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,3 @@
 module.exports = {
   extends: '@antfu',
-  env: {
-    node: true,
-    browser: false,
-  },
-  parserOptions: {
-    project: ['tsconfig.eslint.json'],
-    extraFileExtensions: ['.vue'],
-  },
 }
